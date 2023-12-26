f96195b5dac6c09af533d87fd254b673b5cf496f
leeyebeen-dev <lyb0201190@gmail.com>
2023-12-26 20:55:59 +0900

    [STYLE/#2] Color 추가 및 설정

---
+++ DMU-iOS/DMU-iOS/Color.swift
@@ -0,0 +1,28 @@
+//
+//  Color.swift
+//  DMU-iOS
+//
+//  Created by 이예빈 on 12/26/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let Gray100 = Color("Gray100")
+    static let Gray200 = Color("Gray200")
+    static let Gray300 = Color("Gray300")
+    static let Gray400 = Color("Gray400")
+    static let Gray500 = Color("Gray500")
+    static let Gray600 = Color("Gray600")
+    
+    static let White = Color("White")
+    static let Black = Color("Black")
+    
+    static let Blue100 = Color("Blue100")
+    static let Blue200 = Color("Blue200")
+    static let Blue300 = Color("Blue300")
+    static let Blue400 = Color("Blue400")
+    
+    static let Red100 = Color("Red100")
+}
