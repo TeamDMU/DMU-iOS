0884d8fe09707be9cbd3e478f8d9238aabf79bb8
leeyebeen-dev <lyb0201190@gmail.com>
2023-12-26 21:29:42 +0900

    [STYLE/#2] Font 추가 및 설정

---
+++ DMU-iOS/DMU-iOS/fonts/Font.swift
@@ -0,0 +1,37 @@
+//
+//  Font.swift
+//  DMU-iOS
+//
+//  Created by 이예빈 on 12/26/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    
+    // Bold
+    static let Bold32: Font = .custom("Pretendard-Bold", size: 32)
+    static let Bold24: Font = .custom("Pretendard-Bold", size: 24)
+    static let Bold16: Font = .custom("Pretendard-Bold", size: 16)
+    
+    // SemiBold
+    static let SemiBold20: Font = .custom("Pretendard-SemiBold", size: 20)
+    static let SemiBold16: Font = .custom("Pretendard-SemiBold", size: 16)
+    static let SemiBold15: Font = .custom("Pretendard-SemiBold", size: 15)
+    static let SemiBold14: Font = .custom("Pretendard-SemiBold", size: 14)
+    static let SemiBold13: Font = .custom("Pretendard-SemiBold", size: 13)
+    static let SemiBold12: Font = .custom("Pretendard-SemiBold", size: 12)
+    
+    // Medium
+    static let Medium24: Font = .custom("Pretendard-Medium", size: 24)
+    static let Medium20: Font = .custom("Pretendard-Medium", size: 20)
+    static let Medium18: Font = .custom("Pretendard-Medium", size: 18)
+    static let Medium16: Font = .custom("Pretendard-Medium", size: 16)
+    static let Medium14: Font = .custom("Pretendard-Medium", size: 14)
+    static let Medium12: Font = .custom("Pretendard-Medium", size: 12)
+    
+    // Regular
+    static let Regular12: Font = .custom("Pretendard-Regular", size: 12)
+    
+}
