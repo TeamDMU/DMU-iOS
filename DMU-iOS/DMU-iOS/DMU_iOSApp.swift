d645634d0a9198748fb0d61f301a189a7386e060
leeyebeen-dev <lyb0201190@gmail.com>
2023-12-26 19:36:28 +0900

    [CHORE/#1] 프로젝트 생성 및 환경 구성

---
+++ DMU-iOS/DMU-iOS/DMU_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DMU_iOSApp.swift
+//  DMU-iOS
+//
+//  Created by 이예빈 on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct DMU_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
