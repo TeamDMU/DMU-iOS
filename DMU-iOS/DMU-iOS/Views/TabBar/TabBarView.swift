81dc117c9957d49ab02c17a28db9c9c98cb3e18b
leeyebeen-dev <lyb0201190@gmail.com>
2023-12-31 19:14:17 +0900

    [FEAT/#14] 바텀 탭뷰 구현
    바텀 탭뷰 생성 및 Contentview를 통해 스플래시 이후 홈화면 및 바텀 탭뷰가 보일 수 있게 구현 완료

---
+++ DMU-iOS/DMU-iOS/Views/TabBar/TabBarView.swift
@@ -0,0 +1,90 @@
+//
+//  TabBarView.swift
+//  DMU-iOS
+//
+//  Created by 이예빈 on 12/31/23.
+//
+
+import SwiftUI
+
+class TabBarViewModel: ObservableObject {
+    @Published var selectedTab: Tab = .home
+}
+
+enum Tab: String {
+    case home, search, schedule, diet, settings
+}
+
+struct TabBarView: View {
+    
+    @ObservedObject var viewModel: TabBarViewModel
+    
+    var body: some View {
+        TabView(selection: $viewModel.selectedTab) {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.gray400)
+                    Text("공지")
+                        .font(.Medium12)
+                        .foregroundColor(.gray400)
+                }
+                .tag(Tab.home)
+            
+            SearchView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.gray400)
+                    Text("검색")
+                        .font(.Medium12)
+                        .foregroundColor(.gray400)
+                }
+                .tag(Tab.search)
+            
+            ScheduleView()
+                .tabItem {
+                    Image(systemName: "calendar")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.gray400)
+                    Text("일정")
+                        .font(.Medium12)
+                        .foregroundColor(.gray400)
+                }
+                .tag(Tab.schedule)
+            
+            MealView()
+                .tabItem {
+                    Image(systemName: "fork.knife")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.gray400)
+                    Text("식단")
+                        .font(.Medium12)
+                        .foregroundColor(.gray400)
+                }
+                .tag(Tab.diet)
+            
+            SettingView()
+                .tabItem {
+                    Image(systemName: "gearshape")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.gray400)
+                    Text("설정")
+                        .font(.Medium12)
+                        .foregroundColor(.gray400)
+                }
+                .tag(Tab.settings)
+        }
+        .accentColor(.blue300)
+    }
+}
+
+#Preview {
+    TabBarView(viewModel: TabBarViewModel())
+}
