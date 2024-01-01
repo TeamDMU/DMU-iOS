4f3147a77d4cd9eb693722a0194211ac6122d370
leeyebeen-dev <lyb0201190@gmail.com>
2024-01-01 19:45:20 +0900

    [FEAT/#16] 검색 화면 구현
    검색 화면 구현,
    검색어 입력 시 리스트 뷰 활성화

--- DMU-iOS/DMU-iOS/Views/Search/SearchView.swift
+++ DMU-iOS/DMU-iOS/Views/Search/SearchView.swift
@@ -8,8 +8,103 @@
 import SwiftUI
 
 struct SearchView: View {
+    
+    @State private var searchText = ""
+    @State private var isEditing = false
+    
     var body: some View {
-        Text("search")
+        VStack {
+            HStack {
+                TextField("검색어를 입력하세요.", text: $searchText)
+                    .padding(12)
+                    .padding(.horizontal, 28)
+                    .font(.Medium16)
+                    .foregroundColor(Color.blue300)
+                    .background(Color.blue100)
+                    .cornerRadius(8)
+                    .overlay(
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.blue300)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                .padding(.leading, 12)
+                                .padding(.trailing, 12)
+                            
+                            if isEditing && !searchText.isEmpty {
+                                Button(action: {
+                                    self.searchText = ""
+                                }) {
+                                    Image(systemName: "multiply.circle.fill")
+                                        .foregroundColor(.gray400)
+                                        .padding(.trailing, 12)
+                                }
+                            }
+                        }
+                    )
+                    .padding(.horizontal, 20)
+                    .onTapGesture {
+                        withAnimation {
+                            self.isEditing = true
+                        }
+                    }
+                
+                if isEditing {
+                    Button(action: {
+                        self.isEditing = false
+                        self.searchText = ""
+                        hideKeyboard()
+                    }) {
+                        Text("취소")
+                            .padding(.trailing, 20)
+                            .font(.Medium16)
+                            .foregroundColor(Color.blue300)
+                            .transition(.move(edge: .trailing))
+                    }
+                }
+            }
+            
+            ScrollView {
+                if !searchText.isEmpty {
+                    LazyVStack(alignment: .leading) {
+                        ForEach(sampleData.filter({ "\($0.title)".contains(searchText) || searchText.isEmpty }), id: \.id) { item in
+                            VStack(alignment: .leading) {
+                                Text(item.title)
+                                    .font(.Medium16)
+                                    .foregroundColor(.black)
+                                HStack {
+                                    Text("\(formatDate(item.date))")
+                                        .font(.Regular12)
+                                        .foregroundColor(.gray400)
+                                    
+                                    Text(item.staffName)
+                                        .font(.Regular12)
+                                        .foregroundColor(.gray400)
+                                }
+                                .padding(.top, 1)
+                            }
+                            .padding(16)
+                            .background(Color.white)
+                            .cornerRadius(0)
+                            .shadow(color: .gray, radius: 0, x: 0, y: 0)
+                            
+                            Divider().background(Color.gray200)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+func formatDate(_ date: Date) -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy.MM.dd"
+    return formatter.string(from: date)
+}
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
