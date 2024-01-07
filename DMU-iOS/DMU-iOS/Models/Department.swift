ae3162da83aedef596b6f9752ccd290a4ce4a1e9
leeyebeen-dev <lyb0201190@gmail.com>
2024-01-07 21:48:55 +0900

    [UI/#33] 학과 설정 화면
    학과 선택 리스트뷰 화면 구현

--- DMU-iOS/DMU-iOS/Models/Department.swift
+++ DMU-iOS/DMU-iOS/Models/Department.swift
@@ -9,15 +9,14 @@ import Foundation
 
 
 struct Department {
-    /*
-     static let list = [
+    
+     static let departmentlist = [
         "기계공학과", "기계설계공학과", "로봇공학과", "자동화공학과", "전기공학과", "정보전자공학과",
         "반도체전자공학과", "정보통신공학과", "소방안전관리과", "컴퓨터소프트웨어공학과",
         "컴퓨터정보공학과", "인공지능소프트웨어학과", "생명화학공학과", "바이오융합공학과",
         "건축과", "실내건축디자인과", "시각디자인과", "AR-VR콘텐츠디자인과", "경영학과",
         "세무회계학과", "유통마케팅학과", "호텔관광학과", "경영정보학과", "빅데이터경영과"
     ]
-     */
     
     static let list = [
         "Mechanical Engineering", "Mechanical Design Engineering", "Robotics Engineering", "Automation Engineering", "Electrical Engineering", "Information Electronics Engineering",
--- DMU-iOS/DMU-iOS/Views/Setting/SettingDepartmentView.swift
+++ DMU-iOS/DMU-iOS/Views/Setting/SettingDepartmentView.swift
@@ -9,9 +9,51 @@ import SwiftUI
 
 struct SettingDepartmentView: View {
     @ObservedObject var viewModel: SettingViewModel
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    @State private var selectedDepartment: String? = nil
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(Department.departmentlist, id: \.self) { department in
+                        Text(department)
+                            .font(.Medium18)
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 40, alignment: .center)
+                            .padding([.top, .bottom], 8)
+                            .background(self.selectedDepartment == department ? Color.Blue300 : Color.white)
+                            .foregroundColor(self.selectedDepartment == department ? Color.white : Color.Gray400)
+                            .onTapGesture {
+                                self.selectedDepartment = self.selectedDepartment == department ? nil : department
+                            }
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(self.selectedDepartment == department ? .clear : Color.Gray200, lineWidth: 1)
+                            )
+                    }
+                }
+                .padding(20)
+                .background(Color.Gray100)
+            }
+            .navigationBarBackButtonHidden(true)
+            .navigationBarTitle("학과 설정", displayMode: .inline)
+            .navigationBarItems(leading: Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(systemName: "chevron.backward")
+                    .foregroundColor(Color.Gray500)
+            }, trailing: Button(action: {
+                
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("완료")
+                    .font(.Medium16)
+                    .foregroundColor(self.selectedDepartment != nil ? Color.Blue300 : Color.Gray500)
+            }
+            .disabled(self.selectedDepartment == nil))
+        }
     }
 }
 
