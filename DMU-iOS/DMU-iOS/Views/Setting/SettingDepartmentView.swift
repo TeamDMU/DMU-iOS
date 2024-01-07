03bda69c853653120fa4f1fe67201b3fbdd951f1
leeyebeen-dev <lyb0201190@gmail.com>
2024-01-07 22:35:14 +0900

    [REFACTOR/#33] 학과 설정 화면 코드 개선

--- DMU-iOS/DMU-iOS/Views/Setting/SettingDepartmentView.swift
+++ DMU-iOS/DMU-iOS/Views/Setting/SettingDepartmentView.swift
@@ -14,50 +14,64 @@ struct SettingDepartmentView: View {
     @State private var settingDepartment: String? = nil
     
     var body: some View {
-        VStack {
-            ScrollView {
-                LazyVStack {
-                    ForEach(Department.departmentlist, id: \.self) { department in
-                        Text(department)
-                            .font(.Medium18)
-                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 40, alignment: .center)
-                            .padding([.top, .bottom], 8)
-                            .background(self.settingDepartment == department ? Color.Blue300 : Color.white)
-                            .foregroundColor(self.settingDepartment == department ? Color.white : Color.Gray400)
-                            .onTapGesture {
-                                self.settingDepartment = self.settingDepartment == department ? nil : department
-                            }
-                            .cornerRadius(10)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(self.settingDepartment == department ? .clear : Color.Gray200, lineWidth: 1)
-                            )
-                    }
-                }
-                .padding(20)
-                .background(Color.Gray100)
-            }
-            .navigationBarBackButtonHidden(true)
-            .navigationBarTitle("학과 설정", displayMode: .inline)
-            .navigationBarItems(leading: Button(action: {
-                self.presentationMode.wrappedValue.dismiss()
-            }) {
-                Image(systemName: "chevron.backward")
-                    .foregroundColor(Color.Gray500)
-            }, trailing: Button(action: {
-                // 학과명 저장
-                guard let settingDepartment = self.settingDepartment else {
-                    return
-                }
-                self.viewModel.userSettings.selectedDepartment = settingDepartment
-                self.presentationMode.wrappedValue.dismiss()
-            }) {
-                Text("완료")
-                    .font(.Medium16)
-                    .foregroundColor(self.settingDepartment != nil ? Color.Blue300 : Color.Gray500)
-            }
-                .disabled(self.settingDepartment == nil))
+        departmentList()
+        .navigationBarBackButtonHidden(true)
+        .navigationBarTitle("학과 설정", displayMode: .inline)
+        .navigationBarItems(leading: backButton, trailing: saveButton)
+    }
+    
+    @ViewBuilder
+    var backButton: some View {
+        Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }) {
+            Image(systemName: "chevron.backward")
+                .foregroundColor(Color.Gray500)
         }
+    }
+    
+    @ViewBuilder
+    var saveButton: some View {
+        Button(action: {
+            viewModel.saveDepartment()
+            self.presentationMode.wrappedValue.dismiss()
+        }) {
+            Text("완료")
+                .font(.Medium16)
+                .foregroundColor(viewModel.settingDepartment != nil ? Color.Blue300 : Color.Gray500)
+        }
+        .disabled(viewModel.settingDepartment == nil)
+    }
+    
+    @ViewBuilder
+    func departmentList() -> some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(Department.departmentlist, id: \.self) { department in
+                    departmentRow(for: department)
+                }
+            }
+            .padding(20)
+            .background(Color.Gray100)
+        }
+    }
+    
+    @ViewBuilder
+    func departmentRow(for department: String) -> some View {
+        Text(department)
+            .font(.Medium18)
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 40, alignment: .center)
+            .padding([.top, .bottom], 8)
+            .background(viewModel.settingDepartment == department ? Color.Blue300 : Color.white)
+            .foregroundColor(viewModel.settingDepartment == department ? Color.white : Color.Gray400)
+            .onTapGesture {
+                viewModel.selectDepartment(department)
+            }
+            .cornerRadius(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(viewModel.settingDepartment == department ? .clear : Color.Gray200, lineWidth: 1)
+            )
     }
 }
 
