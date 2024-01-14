417e0a7458b121c45d4ce380a50183048388e570
leeyebeen-dev <lyb0201190@gmail.com>
2024-01-14 18:23:12 +0900

    [REFACTOR/#39] 키워드 알림함 화면 코드 개선

--- DMU-iOS/DMU-iOS/Views/Notification/NotificationView.swift
+++ DMU-iOS/DMU-iOS/Views/Notification/NotificationView.swift
@@ -8,47 +8,14 @@
 import SwiftUI
 
 struct NotificationView: View {
-    @State var notices: [Notice] = sampleData
-    
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @StateObject private var viewModel = NotificationViewModel()
     
     var body: some View {
         ScrollView {
             LazyVStack {
-                ForEach(notices) { notice in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(notice.noticeTitle)
-                                .font(.Medium16)
-                                .foregroundColor(Color.Gray500)
-                                .fixedSize(horizontal: false, vertical: true)
-                            
-                            HStack{
-                                Text(notice.noticeType)
-                                    .font(.SemiBold12)
-                                    .foregroundColor(Color.Gray400)
-                                
-                                Divider()
-                                    .background(Color.Gray300)
-                                
-                                Text(notice.noticeKeyword)
-                                    .font(.SemiBold12)
-                                    .foregroundColor(Color.Gray400)
-                            }
-                            .padding(.top, 0)
-                        }
-                        
-                        Spacer()
-                        
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(Color.Gray400)
-                            .frame(width: 24, height: 24)
-                    }
-                    .padding(20)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.Gray300, lineWidth: 1)
-                    )
+                ForEach(viewModel.notices) { notice in
+                    NotificationListView(notice: notice)
                 }
                 .padding(.bottom, 8)
             }
@@ -59,27 +26,67 @@ struct NotificationView: View {
         }
     }
     
-    @ViewBuilder
-    var homeBackButton: some View {
+    private var homeBackButton: some View {
         Button(action: {
             self.presentationMode.wrappedValue.dismiss()
         }) {
             Image(systemName: "chevron.backward")
-                .foregroundColor(Color.Gray500)
+                .foregroundColor(Color.gray500)
         }
     }
     
-    @ViewBuilder
-    var keywordEditButton: some View {
+    private var keywordEditButton: some View {
         Button(action: {
-            
+            viewModel.editKeywords()
         }) {
             Text("완료")
                 .font(.Medium16)
-                .foregroundColor(Color.Gray500)
+                .foregroundColor(Color.gray500)
         }
     }
 }
+
+// MARK: - 알림 리스트뷰
+struct NotificationListView: View {
+    var notice: Notice
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(notice.noticeTitle)
+                    .font(.Medium16)
+                    .foregroundColor(Color.gray500)
+                    .fixedSize(horizontal: false, vertical: true)
+                
+                HStack {
+                    Text(notice.noticeType)
+                        .font(.SemiBold12)
+                        .foregroundColor(Color.gray400)
+                    
+                    Divider()
+                        .background(Color.gray300)
+                    
+                    Text(notice.noticeKeyword)
+                        .font(.SemiBold12)
+                        .foregroundColor(Color.gray400)
+                }
+                .padding(.top, 0)
+            }
+            
+            Spacer()
+            
+            Image(systemName: "chevron.right")
+                .foregroundColor(Color.gray400)
+                .frame(width: 24, height: 24)
+        }
+        .padding(20)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.gray300, lineWidth: 1)
+        )
+    }
+}
+
 
 #Preview {
     NotificationView()
