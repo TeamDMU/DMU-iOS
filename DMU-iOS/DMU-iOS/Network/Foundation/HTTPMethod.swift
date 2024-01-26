06c24eb119e378147a9d26fa231361807a8982a5
leeyebeen-dev <lyb0201190@gmail.com>
2024-01-26 10:48:27 +0900

    [CHORE/#47] HTTPMethod.swift 파일 생성 및 HTTP 메소드 타입 선언

---
+++ DMU-iOS/DMU-iOS/Network/Foundation/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  DMU-iOS
+//
+//  Created by 이예빈 on 1/26/24.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
