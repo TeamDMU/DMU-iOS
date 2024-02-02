02b0ca6846b4b5daf24a40575de67532c8316d56
leeyebeen-dev <lyb0201190@gmail.com>
2024-02-02 10:50:04 +0900

    [NETWORK/#51] 식단표 Repository 추가

---
+++ DMU-iOS/DMU-iOS/Features/Meal/Models/Repository/MenuRepository.swift
@@ -0,0 +1,31 @@
+//
+//  MenuRepository.swift
+//  DMU-iOS
+//
+//  Created by 이예빈 on 2/2/24.
+//
+
+import Foundation
+
+import Moya
+
+class MenuRepository {
+    let provider = MoyaProvider<APIService>()
+
+    func getMenus(completion: @escaping (Result<[Menu], Error>) -> Void) {
+        provider.request(.getMenus) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let menuDTOs = try JSONDecoder().decode([MenuDTO].self, from: response.data)
+                    let menus = menuDTOs.flatMap { $0.toMenus() }
+                    completion(.success(menus))
+                } catch {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
