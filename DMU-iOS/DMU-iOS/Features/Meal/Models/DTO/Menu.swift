af2c71f9427ce3a523ec8b88abed184fbd2c1fe6
leeyebeen-dev <lyb0201190@gmail.com>
2024-02-11 17:13:12 +0900

    [CHORE/#63] Meal 뷰, 뷰모델 초기화

--- DMU-iOS/DMU-iOS/Features/Meal/Models/DTO/Menu.swift
+++ DMU-iOS/DMU-iOS/Features/Meal/Models/DTO/Menu.swift
@@ -13,3 +13,16 @@ struct Menu: Identifiable {
     var date: String
     var details: [String]
 }
+
+let sampleMenu = [
+    Menu(date: "2024-02-05", details: ["없음"]),
+    Menu(date: "2024-02-06", details: ["백미밥","제육볶음","타코야끼","양상추발사믹샐러드","배추김치","부대찌개"]),
+    Menu(date: "2024-02-07", details: ["없음"]),
+    Menu(date: "2024-02-08", details: ["없음"]),
+    Menu(date: "2024-02-09", details: ["없음"]),
+    Menu(date: "2024-02-10", details: ["없음"]),
+    Menu(date: "2024-02-11", details: ["백미밥","제육볶음","타코야끼","양상추발사믹샐러드","배추김치","부대찌개"]),
+    Menu(date: "2024-02-14", details: ["없음"]),
+    Menu(date: "2024-02-15", details: ["없음"]),
+    Menu(date: "2024-02-16", details: ["없음"]),
+]
--- DMU-iOS/DMU-iOS/Features/Meal/Views/MealView.swift
+++ DMU-iOS/DMU-iOS/Features/Meal/Views/MealView.swift
@@ -9,72 +9,13 @@ import SwiftUI
 
 struct MealView: View {
     
-    @StateObject var viewModel: MealViewModel
-    
-    init(viewModel: MealViewModel = MealViewModel()) {
-        _viewModel = StateObject(wrappedValue: viewModel)
-    }
-    
     var body: some View {
         VStack {
-            MealTitle
             
-            MenuListView
-        }
-        .onAppear(perform: viewModel.loadData)
-    }
-    
-    // MARK: 금주의 식단 화면 타이틀 뷰
-    private var MealTitle: some View {
-        Text("금주의 식단")
-            .font(.SemiBold20)
-            .padding()
-            .frame(maxWidth: .infinity)
-            .foregroundColor(Color.Gray500)
-            .padding(.horizontal, 20)
-    }
-    
-    // MARK: 금주의 식단 메뉴 리스트 뷰
-    private var MenuListView: some View {
-        ScrollView {
-            VStack(alignment: .center, spacing: 0) {
-                ForEach(viewModel.thisWeeksMenu) { menu in
-                    MenuDateView(date: viewModel.formatDate(menu.date))
-                        .padding(.horizontal, 20)
-                    
-                    Text(menu.details.joined(separator: ", "))
-                        .font(.SemiBold16)
-                        .foregroundColor(Color.Gray500)
-                        .multilineTextAlignment(.center)
-                        .padding(.vertical, 14)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .padding(.bottom, 20)
-                }
-            }
         }
     }
 }
 
-struct MenuDateView: View {
-    
-    var date: String
-    
-    var body: some View {
-        HStack {
-            Spacer(minLength: 20)
-            
-            Text(date)
-                .font(.SemiBold16)
-                .padding(.vertical, 10)
-                .padding(.horizontal, 20)
-                .background(Color.Blue100)
-                .foregroundColor(Color.Gray500)
-                .cornerRadius(30)
-            
-            Spacer(minLength: 20)
-        }
-    }
-}
 #Preview {
     MealView()
 }
