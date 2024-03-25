b715f23ba3328d2e277aa37e567ad440af065e9e
leeyebeen-dev <lyb0201190@gmail.com>
2024-03-25 12:57:57 +0900

    [CHORE/#92] 학과 설정 화면에 기존 선택 학과명 불러오기

--- DMU-iOS/DMU-iOS/Features/Setting/ViewModels/SettingViewModel.swift
+++ DMU-iOS/DMU-iOS/Features/Setting/ViewModels/SettingViewModel.swift
@@ -14,6 +14,7 @@ class SettingViewModel: ObservableObject {
     
     init(userSettings: UserSettings) {
         self.userSettings = userSettings
+        self.settingDepartment = userSettings.selectedDepartment
     }
     
     //MARK: 학과 선택
